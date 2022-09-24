0ec6774eb9e6f3022d8d8c6345a56160d755e78f
Richie-11 <ghaleyrichfield@gmail.com>
2022-09-24 20:17:29 +0100

    my

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=Richness1
+export POSTGRES_PASSWORD=Jottagha12
+export POSTGRES_HOST=richness.cnxde6zzwclu.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=richness
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
